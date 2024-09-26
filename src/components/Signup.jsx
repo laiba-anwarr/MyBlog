cd50ef738815abbcddd2197fd610c175fa174410
laiba anwar <TheTechScripter@gmail.com>
2024-09-27 00:24:06 +0500

    signup local storage

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -24,6 +24,7 @@ function Signup() {
       if (session) {
         const userData = await authService.getCurrentUser();
         if (userData) dispatch(login(userData));
+        localStorage.setItem("userData", JSON.stringify(userData));
         setLoading(false)
       navigate('/')
       }
